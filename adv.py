17f5cc2f9fcff143e186b156d26cb455765f0280
Raymond Thurman <raymondthurman5@gmail.com>
2020-08-28 22:07:50 -0400

    ✨ import stack and queue, check neighbor room func

--- adv.py
+++ adv.py
@@ -4,6 +4,7 @@ from world import World
 
 import random
 from ast import literal_eval
+from utils import Stack, Queue
 
 # Load world
 world = World()
@@ -17,7 +18,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -28,7 +29,23 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+visited_graph = {}
 
+
+def check_neighbor_rooms(current_room, next_room):
+    if current_room == next_room:
+        return 'Same Room'
+    elif current_room.n_to == next_room:
+        return 'n'
+    elif current_room.s_to == next_room:
+        return 's'
+    elif current_room.w_to == next_room:
+        return 'w'
+    elif current_room.e_to == next_room:
+        return 'e'
+
+# IF no adjacent room, BFS to find the shortest path to the
+# next new room
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -41,11 +58,11 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
